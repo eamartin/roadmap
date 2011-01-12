7c5f364e242b020c6165b4a98b3d4ac2f27be7ae
Eric Martin <e.a.martin1337@gmail.com>
2011-01-12 10:34:40 +0000

    Converted to complete coroutine base. Tests need to be rewritten.

--- roadmap.py
+++ roadmap.py
@@ -2,56 +2,76 @@
 
 import re
 
-class Roadmap(dict):
+def coroutine(func):
+    def start(*args, **kwargs):
+        cr = func(*args, **kwargs)
+        cr.next()
+        return cr
+    return start
 
-    def default(self):
-        pass
+class Router(dict):
+
+    def __init__(self):
+        self.find_match_target = self.find_match()
 
     def destination(self, reg_str, pass_obj=True):
-        '''Maps a regular expression to a function in the internal dictionary
-        of the Roadmap instance.
-
-        This method should be primarily used as a decorator with parameters.
-        The ``reg_str`` parameter is a raw string that will be made into a
-        regular expression. ``pass_obj`` determines if the object passed to
-        Roadmap.route will be passed to the function.
-        '''
         regex = re.compile(reg_str)
 
         def decorator(func):
             self[regex] = {'func': func, 'pass_obj': pass_obj}
             return func
-
         return decorator
-
-    def handle_match(self, match, obj):
-        groups = match.groups()
-        groupdict = match.groupdict()
-
-        if groupdict:
-            if len(groupdict) == len(groups):
-                return self.process_pair(match.re, obj, **groupdict)
-            else:
-                exclusives = [s for s in groups if s not in groupdict.values()]
-                return self.process_pair(match.re, obj, *exclusives, **groupdict)
-        else:
-            return self.process_pair(match.re, obj, *groups)
-
-    def process_pair(self, regex, obj, *args, **kwargs):
-        if self[regex]['pass_obj']:
-            if hasattr(obj, '__iter__'):
-                objects = [o for o in obj]
-                objects.extend(args)
-                return self[regex]['func'](*objects, **kwargs)
-            else:
-                return self[regex]['func'](obj, *args, **kwargs)
-        else:
-            return self[regex]['func'](*args, **kwargs)
 
     def route(self, obj, key=None):
         string = key or obj
-        for regex in self.keys():
-            match = regex.match(string)
-            if match:
-                return self.handle_match(match, obj)
-        return self.default()
+        self.find_match_target.send((obj, string))
+
+    @coroutine
+    def find_match(self):
+        handle_match = self.handle_match()
+        while True:
+            obj, string = (yield)
+            for regex in self.keys():
+                match = regex.match(string)
+                if match:
+                    handle_match.send((match, obj))
+
+    @coroutine
+    def handle_match(self):
+        process_pair = self.process_pair()
+        while True:
+            match, obj = (yield)
+
+            groups = match.groups()
+            groupdict = match.groupdict()
+
+            if groupdict:
+                if len(groupdict) == len(groups):
+                    process_pair.send((match.re, obj, (), groupdict))
+                else:
+                    exclusives = [s for s in groups if s not in groupdict.values()]
+                    process_pair.send((match.re, obj, exclusives, groupdict))
+            else:
+                process_pair.send((match.re, obj, groups, {}))
+
+    @coroutine
+    def process_pair(self):
+        try:
+            while True:
+                parameters = (yield)
+                regex = parameters[0]
+                obj = parameters[1]
+                args = parameters[2]
+                kwargs = parameters[3]
+
+                if self[regex]['pass_obj']:
+                    if hasattr(obj, '__iter__'):
+                        objects = [o for o in obj]
+                        objects.extend(args)
+                        self[regex]['func'](*objects, **kwargs)
+                    else:
+                        self[regex]['func'](obj, *args, **kwargs)
+                else:
+                    self[regex]['func'](*args, **kwargs)
+        except GeneratorExit:
+            pass
